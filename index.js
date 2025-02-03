889ca639754d079e535a2012d10a3c345191caeb
aoge2716 <aoge2716@gmail.com>
2025-02-03 16:48:40 +0100

    implemented all methods for SortedList according to the request

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b); // sort added array
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.length< pos){
+      throw new Error("OutOfBounds");
+    }
+    else{
+      return this.items[pos];
+    }
+    
+  }
 
-  max() {}
+  max() {
+    if(!this.items.length){
+      throw new Error("EmptySortedList");
+    }else{
+      const max = this.items.reduce((acc,curr)=>Math.max(acc,curr),-Infinity);
+      return max;
+    }
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length){
+      throw new Error("EmptySortedList");
+    }else{
+      const min = this.items.reduce((acc,curr)=>Math.min(acc,curr), Infinity)
+      return min;
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(!this.items.length){
+      return 0;
+    }else{
+      const sum = this.items.reduce((acc,curr)=>acc+curr,0);
+      return sum
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(!this.items.length){
+      throw new Error("EmptySortedList");
+    }else{
+      return (this.sum()/this.items.length);
+    }
+  }
 }
 
 module.exports = SortedList;
+
